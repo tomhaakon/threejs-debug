24d9902d34359e6554c6928b308f46fff614d98f
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-17 00:29:03 +0200

    init

--- main.js
+++ main.js
@@ -7,62 +7,118 @@ import { sendStatus } from './handleStatus.js'
 
 sendError('loaded', 'main.js') // send msg that main.js is loaded
 
+class ClearingLogger {
+  constructor(elem) {
+    this.elem = elem
+    this.lines = []
+  }
+  log(...args) {
+    this.lines.push([...args].join(' '))
+  }
+  render() {
+    this.elem.textContent = this.lines.join('\n')
+    this.lines = []
+  }
+}
+
 function main() {
-  //
-  //canvas
   const canvas = document.querySelector('#c')
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
 
-  //camera
-  const fov = 45
-  const aspect = 2
+  const fov = 75
+  const aspect = 2 // the canvas default
   const near = 0.1
-  const far = 100
+  const far = 50
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-  camera.position.set(0, 10, 20)
+  camera.position.z = 20
 
-  //controls
-  const controls = new OrbitControls(camera, canvas)
-  controls.target.set(0, 5, 0)
-  controls.update()
-
-  //scene
   const scene = new THREE.Scene()
-  scene.background = new THREE.Color('black')
+  scene.background = new THREE.Color('cyan')
 
-  /*
-  
-  innhold
-  
-  */
+  const geometry = new THREE.SphereGeometry()
+  const material = new THREE.MeshBasicMaterial({ color: 'red' })
 
-  //render functinons
- function resizeRendererToDisplaySize(renderer) {
-    //
+  const things = []
+
+  function rand(min, max) {
+    if (max === undefined) {
+      max = min
+      min = 0
+    }
+
+    return Math.random() * (max - min) + min
+  }
+
+  function createThing() {
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+    things.push({
+      mesh,
+      timer: 2,
+      velocity: new THREE.Vector3(rand(-5, 5), rand(-5, 5), rand(-5, 5)),
+    })
+  }
+
+  canvas.addEventListener('click', createThing)
+
+  function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement
     const width = canvas.clientWidth
     const height = canvas.clientHeight
     const needResize = canvas.width !== width || canvas.height !== height
     if (needResize) {
-      //
       renderer.setSize(width, height, false)
     }
+
     return needResize
   }
-  function render() {
+
+  const logger = new ClearingLogger(document.querySelector('#debug pre'))
+
+  let then = 0
+  function render(now) {
+    now *= 0.001 // convert to seconds
+    const deltaTime = now - then
+    then = now
+
     if (resizeRendererToDisplaySize(renderer)) {
-      //
       const canvas = renderer.domElement
       camera.aspect = canvas.clientWidth / canvas.clientHeight
       camera.updateProjectionMatrix()
     }
+
+    logger.log('fps:', (1 / deltaTime).toFixed(1))
+    logger.log('num things:', things.length)
+    for (let i = 0; i < things.length; ) {
+      const thing = things[i]
+      const mesh = thing.mesh
+      const pos = mesh.position
+      logger.log(
+        'timer:',
+        thing.timer.toFixed(3),
+        'pos:',
+        pos.x.toFixed(3),
+        pos.y.toFixed(3),
+        pos.z.toFixed(3)
+      )
+      thing.timer -= deltaTime
+      if (thing.timer <= 0) {
+        // remove this thing. Note we don't advance `i`
+        things.splice(i, 1)
+        scene.remove(mesh)
+      } else {
+        mesh.position.addScaledVector(thing.velocity, deltaTime)
+        ++i
+      }
+    }
+
     renderer.render(scene, camera)
+    logger.render()
 
     requestAnimationFrame(render)
   }
+
   requestAnimationFrame(render)
-  //
-  sendStatus(true) // icon that shows that main functio is running
-  //
 }
+
 main()
